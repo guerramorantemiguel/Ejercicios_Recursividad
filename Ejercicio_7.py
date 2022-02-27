325a5eaeeaafe6fd3fdf7ae760bdfa5e42c24e02
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-02-27 16:40:32 +0000

    desarrollo código

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -6,3 +6,14 @@ a = len(color) - 1
 colores = []
 for j in range(0, a+1):
   colores.append(color[j:j+1])
+while(v<=a):
+  if(colores[v] == 'R'):
+    v = v + 1
+  else:
+    if(colores[v] == 'R'):
+      aux = colores[v]
+      colores[v] = colores[r]
+      colores[r] = aux
+      v = v + 1
+      r = r + 1
+      
