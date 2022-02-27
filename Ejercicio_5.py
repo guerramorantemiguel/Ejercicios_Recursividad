0e395c73215c12b785d6d7791190c6087109aab6
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-02-27 19:34:23 +0000

    código

---
+++ Ejercicio_5.py
@@ -0,0 +1,10 @@
+tabla = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+a = int(inpu('Introduzca el valor que quiera enontrar '))
+b = 0
+c = len(tabla)
+d = (b + c)/2
+class tablas:
+  def __init__(self,tabla,a,b,c):
+    self.c = c
+    self.a = a
+    self.b = b
